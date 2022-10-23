bb76228ef74e43f8bc3837d73c25934d634e268b
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-23 15:20:57 -0400

    registered the Visit model to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Visit
 # Register your models here.
 
 admin.site.register(Place)
+
+# register the new Visit Model
+admin.site.register(Visit)
+
